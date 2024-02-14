39ce457c41a36821ab5175c512864b831915cd84
Cédric Luthi <cedric.luthi@gmail.com>
2024-02-14 10:05:24 +0100

    feat: Add convenience methods to perform Postgres initialization scripts

--- tests/Testcontainers.PostgreSql.Tests/PostgreSqlContainerTest.cs
+++ tests/Testcontainers.PostgreSql.Tests/PostgreSqlContainerTest.cs
@@ -1,8 +1,13 @@
 namespace Testcontainers.PostgreSql;
 
-public sealed class PostgreSqlContainerTest : IAsyncLifetime
+public abstract class PostgreSqlContainerTest : IAsyncLifetime
 {
-    private readonly PostgreSqlContainer _postgreSqlContainer = new PostgreSqlBuilder().Build();
+    private readonly PostgreSqlContainer _postgreSqlContainer;
+
+    protected PostgreSqlContainerTest(PostgreSqlContainer postgreSqlContainer)
+    {
+        _postgreSqlContainer = postgreSqlContainer;
+    }
 
     public Task InitializeAsync()
     {
@@ -73,6 +78,47 @@ public sealed class PostgreSqlContainerTest : IAsyncLifetime
                 .ConfigureAwait(true);
 
             Assert.False(_cts.IsCancellationRequested);
+        }
+    }
+
+    [UsedImplicitly]
+    public sealed class PostgreSqlDefaultConfiguration : PostgreSqlContainerTest
+    {
+        public PostgreSqlDefaultConfiguration()
+            : base(new PostgreSqlBuilder().Build())
+        {
+        }
+    }
+
+    [UsedImplicitly]
+    public sealed class PostgreSqlChinookInitialization : PostgreSqlContainerTest
+    {
+        public PostgreSqlChinookInitialization()
+            : base(new PostgreSqlBuilder()
+                .WithInitializationScripts(
+                    new Uri("https://github.com/lerocha/chinook-database/releases/download/v1.4.5/Chinook_PostgreSql.sql"),
+                    new Uri("https://github.com/lerocha/chinook-database/releases/download/v1.4.5/Chinook_PostgreSql_AutoIncrementPKs.sql"))
+                .Build())
+        {
+        }
+
+        [Theory]
+        [InlineData("chinook")]
+        [InlineData("chinook_auto_increment")]
+        [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
+        public void ChinookDatabaseIsInitialized(string database)
+        {
+            // Given
+            var connectionStringBuilder = new NpgsqlConnectionStringBuilder(_postgreSqlContainer.GetConnectionString()) { Database = database };
+            using var connection = new NpgsqlConnection(connectionStringBuilder.ConnectionString);
+            using var command = new NpgsqlCommand("SELECT name FROM genre WHERE genre_id = 1", connection);
+            connection.Open();
+
+            // When
+            var result = command.ExecuteScalar();
+
+            // Then
+            Assert.Equal("Rock", result);
         }
     }
 

57b5591655a7e0fb289e708757c98c7ee9edbc6e
Cédric Luthi <cedric.luthi@gmail.com>
2024-05-27 22:19:21 +0200

    Use ContainerTest for MongoDbContainerTest

--- tests/Testcontainers.MongoDb.Tests/MongoDbContainerTest.cs
+++ tests/Testcontainers.MongoDb.Tests/MongoDbContainerTest.cs
@@ -1,34 +1,15 @@
 namespace Testcontainers.MongoDb;
 
-public abstract class MongoDbContainerTest : IAsyncLifetime
+public abstract class MongoDbContainerTest(ITestOutputHelper testOutputHelper, Func<MongoDbBuilder, MongoDbBuilder> configure = null, bool replicaSetEnabled = false)
+    : ContainerTest<MongoDbBuilder, MongoDbContainer>(testOutputHelper, configure)
 {
-    private readonly MongoDbContainer _mongoDbContainer;
-
-    private readonly bool _replicaSetEnabled;
-
-    private MongoDbContainerTest(MongoDbContainer mongoDbContainer, bool replicaSetEnabled = false)
-    {
-        _mongoDbContainer = mongoDbContainer;
-        _replicaSetEnabled = replicaSetEnabled;
-    }
-
     // # --8<-- [start:UseMongoDbContainer]
-    public Task InitializeAsync()
-    {
-        return _mongoDbContainer.StartAsync();
-    }
-
-    public Task DisposeAsync()
-    {
-        return _mongoDbContainer.DisposeAsync().AsTask();
-    }
-
     [Fact]
     [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
     public void ConnectionStateReturnsOpen()
     {
         // Given
-        var client = new MongoClient(_mongoDbContainer.GetConnectionString());
+        var client = new MongoClient(Container.GetConnectionString());
 
         // When
         using var databases = client.ListDatabases();
@@ -45,7 +26,7 @@ public abstract class MongoDbContainerTest : IAsyncLifetime
         const string scriptContent = "printjson(db.adminCommand({listDatabases:1,nameOnly:true,filter:{\"name\":/^admin/}}));";
 
         // When
-        var execResult = await _mongoDbContainer.ExecScriptAsync(scriptContent)
+        var execResult = await Container.ExecScriptAsync(scriptContent)
             .ConfigureAwait(true);
 
         // Then
@@ -62,11 +43,11 @@ public abstract class MongoDbContainerTest : IAsyncLifetime
         const string scriptContent = "rs.status().ok;";
 
         // When
-        var execResult = await _mongoDbContainer.ExecScriptAsync(scriptContent)
+        var execResult = await Container.ExecScriptAsync(scriptContent)
             .ConfigureAwait(true);
 
         // Then
-        if (_replicaSetEnabled)
+        if (replicaSetEnabled)
         {
             Assert.True(0L.Equals(execResult.ExitCode), execResult.Stderr);
             Assert.Empty(execResult.Stderr);
@@ -80,59 +61,29 @@ public abstract class MongoDbContainerTest : IAsyncLifetime
 
     // # --8<-- [start:CreateMongoDbContainer]
     [UsedImplicitly]
-    public sealed class MongoDbDefaultConfiguration : MongoDbContainerTest
-    {
-        public MongoDbDefaultConfiguration()
-            : base(new MongoDbBuilder().Build())
-        {
-        }
-    }
+    public sealed class MongoDbDefaultConfiguration(ITestOutputHelper testOutputHelper)
+        : MongoDbContainerTest(testOutputHelper);
 
     [UsedImplicitly]
-    public sealed class MongoDbNoAuthConfiguration : MongoDbContainerTest
-    {
-        public MongoDbNoAuthConfiguration()
-            : base(new MongoDbBuilder().WithUsername(string.Empty).WithPassword(string.Empty).Build())
-        {
-        }
-    }
+    public sealed class MongoDbNoAuthConfiguration(ITestOutputHelper testOutputHelper)
+        : MongoDbContainerTest(testOutputHelper, builder => builder.WithUsername(string.Empty).WithPassword(string.Empty));
     // # --8<-- [end:CreateMongoDbContainer]
 
     [UsedImplicitly]
-    public sealed class MongoDbV5Configuration : MongoDbContainerTest
-    {
-        public MongoDbV5Configuration()
-            : base(new MongoDbBuilder().WithImage("mongo:5.0").Build())
-        {
-        }
-    }
+    public sealed class MongoDbV5Configuration(ITestOutputHelper testOutputHelper)
+        : MongoDbContainerTest(testOutputHelper, builder => builder.WithImage("mongo:5.0"));
 
     [UsedImplicitly]
-    public sealed class MongoDbV4Configuration : MongoDbContainerTest
-    {
-        public MongoDbV4Configuration()
-            : base(new MongoDbBuilder().WithImage("mongo:4.4").Build(), true /* Replica set status returns "ok" in MongoDB 4.4 without initialization. */)
-        {
-        }
-    }
+    public sealed class MongoDbV4Configuration(ITestOutputHelper testOutputHelper)
+        : MongoDbContainerTest(testOutputHelper, builder => builder.WithImage("mongo:4.4"), replicaSetEnabled: true /* Replica set status returns "ok" in MongoDB 4.4 without initialization. */);
 
     [UsedImplicitly]
-    public sealed class MongoDbReplicaSetDefaultConfiguration : MongoDbContainerTest
-    {
-        public MongoDbReplicaSetDefaultConfiguration()
-            : base(new MongoDbBuilder().WithReplicaSet().Build(), true)
-        {
-        }
-    }
+    public sealed class MongoDbReplicaSetDefaultConfiguration(ITestOutputHelper testOutputHelper)
+        : MongoDbContainerTest(testOutputHelper, builder => builder.WithReplicaSet(), replicaSetEnabled: true);
 
     // # --8<-- [start:ReplicaSetContainerConfiguration]
     [UsedImplicitly]
-    public sealed class MongoDbNamedReplicaSetConfiguration : MongoDbContainerTest
-    {
-        public MongoDbNamedReplicaSetConfiguration()
-            : base(new MongoDbBuilder().WithReplicaSet("rs1").Build(), true)
-        {
-        }
-    }
+    public sealed class MongoDbNamedReplicaSetConfiguration(ITestOutputHelper testOutputHelper)
+        : MongoDbContainerTest(testOutputHelper, builder => builder.WithReplicaSet("rs1"), replicaSetEnabled: true);
     // # --8<-- [end:ReplicaSetContainerConfiguration]
 }

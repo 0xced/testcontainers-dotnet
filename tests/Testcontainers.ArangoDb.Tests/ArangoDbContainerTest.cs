c1344399b38e91ed851994d69a8e96f83db6f81b
Cédric Luthi <cedric.luthi@gmail.com>
2024-08-14 09:35:36 +0200

    Use ContainerTest for ArangoDbContainerTest

--- tests/Testcontainers.ArangoDb.Tests/ArangoDbContainerTest.cs
+++ tests/Testcontainers.ArangoDb.Tests/ArangoDbContainerTest.cs
@@ -1,25 +1,13 @@
 namespace Testcontainers.ArangoDb;
 
-public sealed class ArangoDbContainerTest : IAsyncLifetime
+public sealed class ArangoDbContainerTest(ITestOutputHelper testOutputHelper) : ContainerTest<ArangoDbBuilder, ArangoDbContainer>(testOutputHelper)
 {
-    private readonly ArangoDbContainer _arangoDbContainer = new ArangoDbBuilder().Build();
-
-    public Task InitializeAsync()
-    {
-        return _arangoDbContainer.StartAsync();
-    }
-
-    public Task DisposeAsync()
-    {
-        return _arangoDbContainer.DisposeAsync().AsTask();
-    }
-
     [Fact]
     [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
     public async Task RetrievesDatabases()
     {
         // Given
-        var address = new Uri(_arangoDbContainer.GetTransportAddress());
+        var address = new Uri(Container.GetTransportAddress());
 
         using var transport = HttpApiTransport.UsingBasicAuth(address, ArangoDbBuilder.DefaultUsername, ArangoDbBuilder.DefaultPassword);
 

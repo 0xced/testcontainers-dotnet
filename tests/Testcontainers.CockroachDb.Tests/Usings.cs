c8d460c5368d15cbcde7cb57866b8654e805f121
Cédric Luthi <cedric.luthi@gmail.com>
2024-11-23 15:58:21 +0100

    Use DbContainerFixture for CockroachDbContainerTest

--- tests/Testcontainers.CockroachDb.Tests/Usings.cs
+++ tests/Testcontainers.CockroachDb.Tests/Usings.cs
@@ -3,4 +3,6 @@ global using System.Data.Common;
 global using System.Threading.Tasks;
 global using DotNet.Testcontainers.Commons;
 global using Npgsql;
+global using Testcontainers.Xunit;
 global using Xunit;
+global using Xunit.Abstractions;

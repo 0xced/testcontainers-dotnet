963e930e4810e51c48febcb91619de84daf3ce56
Cédric Luthi <cedric.luthi@gmail.com>
2024-11-24 20:36:53 +0100

    Use ContainerTest for MilvusContainerTest

--- tests/Testcontainers.Milvus.Tests/Usings.cs
+++ tests/Testcontainers.Milvus.Tests/Usings.cs
@@ -1,7 +1,8 @@
 global using System.Threading.Tasks;
 global using DotNet.Testcontainers.Builders;
 global using DotNet.Testcontainers.Commons;
-global using DotNet.Testcontainers.Networks;
 global using JetBrains.Annotations;
 global using Milvus.Client;
+global using Testcontainers.Xunit;
 global using Xunit;
+global using Xunit.Abstractions;

12198c7b497a2ae154ec81572463443a1c66b40c
Cédric Luthi <cedric.luthi@gmail.com>
2024-11-24 20:35:34 +0100

    Use ContainerTest for InfluxDbContainerTest

--- tests/Testcontainers.InfluxDb.Tests/InfluxDbContainerTest.cs
+++ tests/Testcontainers.InfluxDb.Tests/InfluxDbContainerTest.cs
@@ -1,27 +1,15 @@
 namespace Testcontainers.InfluxDb;
 
-public sealed class InfluxDbContainerTest : IAsyncLifetime
+public sealed class InfluxDbContainerTest(ITestOutputHelper testOutputHelper) : ContainerTest<InfluxDbBuilder, InfluxDbContainer>(testOutputHelper)
 {
     private const string AdminToken = "YOUR_API_TOKEN";
-
-    private readonly InfluxDbContainer _influxDbContainer = new InfluxDbBuilder().WithAdminToken(AdminToken).Build();
-
-    public Task InitializeAsync()
-    {
-        return _influxDbContainer.StartAsync();
-    }
-
-    public Task DisposeAsync()
-    {
-        return _influxDbContainer.DisposeAsync().AsTask();
-    }
 
     [Fact]
     [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
     public async Task PingReturnsTrue()
     {
         // Given
-        using var client = new InfluxDBClient(_influxDbContainer.GetAddress(), AdminToken);
+        using var client = new InfluxDBClient(Container.GetAddress(), AdminToken);
 
         // When
         var result = await client.PingAsync()
@@ -44,7 +32,7 @@ public sealed class InfluxDbContainerTest : IAsyncLifetime
 
         const double temperature = 55d;
 
-        using var client = new InfluxDBClient(_influxDbContainer.GetAddress(), AdminToken);
+        using var client = new InfluxDBClient(Container.GetAddress(), AdminToken);
         var queryApi = client.GetQueryApi();
         var writeApi = client.GetWriteApiAsync();
 

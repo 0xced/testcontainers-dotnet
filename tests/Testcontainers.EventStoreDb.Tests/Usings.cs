e2b30c7fbf98dae15d810caa43e3f869d469f477
Cédric Luthi <cedric.luthi@gmail.com>
2024-11-24 20:35:14 +0100

    Use ContainerTest for EventStoreDbContainerTest

--- tests/Testcontainers.EventStoreDb.Tests/Usings.cs
+++ tests/Testcontainers.EventStoreDb.Tests/Usings.cs
@@ -3,4 +3,6 @@ global using System.Linq;
 global using System.Threading.Tasks;
 global using DotNet.Testcontainers.Commons;
 global using EventStore.Client;
+global using Testcontainers.Xunit;
 global using Xunit;
+global using Xunit.Abstractions;

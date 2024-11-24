35e17c2764919210bac6245f65a9f5c2da1d6be5
Cédric Luthi <cedric.luthi@gmail.com>
2024-11-24 20:34:47 +0100

    Use ContainerTest for CouchbaseContainerTest <Couchbase>

--- tests/Testcontainers.Couchbase.Tests/Usings.cs
+++ tests/Testcontainers.Couchbase.Tests/Usings.cs
@@ -2,4 +2,6 @@ global using System.Linq;
 global using System.Threading.Tasks;
 global using Couchbase;
 global using DotNet.Testcontainers.Commons;
+global using Testcontainers.Xunit;
 global using Xunit;
+global using Xunit.Abstractions;
